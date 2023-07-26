cdc69a1a3f7b4d066ba6739ad57b7c9f018d6226
Horatio <hs@horatio.cn>
2023-07-26 22:50:13 +0800

    注释修改为中文并格式化，上线

--- index.php
+++ index.php
@@ -11,35 +11,52 @@
   <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="css/style.min.css" rel="stylesheet" type="text/css">
   <style>
-    /* Set the page height to 100vh (viewport height) */
     body, html {
       height: 100%;
       margin: 0;
       padding: 0;
     }
-    /* Set the section height to 100vh and remove margin and padding */
+
     section {
       height: 100vh;
       margin: 0;
       padding: 0;
     }
-    /* Set background color to white */
+
     body {
       background-color: #fff;
     }
-    /* Footer styling */
+
     footer {
-      background-color: #fff; /* Changed background color to white */
+      background-color: #fff;
       padding: 20px;
       text-align: center;
       position: absolute;
       bottom: 0;
       width: 100%;
     }
+
+    .team-member {
+      text-align: center;
+      margin: 20px;
+      transition: transform 0.2s ease;
+    }
+
+    .team-member img {
+      max-width: 100px;
+      border-radius: 20px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    .team-member:hover img {
+      transform: scale(1.1);
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    }
   </style>
 </head>
+
 <body>
-  <!--Navbar Start-->
+  <!--导航栏开始-->
   <nav class="navbar navbar-expand-lg fixed-top navbar-custom sticky sticky-dark align-items-center">
     <div class="container">
       <!-- LOGO -->
@@ -53,27 +70,70 @@
           <li class="nav-item"><a href="#about" class="nav-link"></a></li>
           <li class="nav-item"><a href="#services" class="nav-link"></a></li>
         </ul>
+        <!-- 添加选择语言按钮 -->
+        <div class="navbar-right">
+          <button class="btn btn-primary">选择语言</button>
+        </div>
       </div>
     </div>
   </nav>
-  <!-- Navbar End -->
-  <!-- Hero Start -->
-  <section class="hero-1-bg bg-white"> <!-- Changed background color back to white -->
+  <!-- 导航栏结束 -->
+  <!-- 主体部分开始 -->
+  <section class="hero-1-bg bg-white"> <!-- 将背景颜色改为白色 -->
     <div class="container">
       <div class="row align-items-center justify-content-center">
         <div class="col-lg-6">
-          <h1 class="display-4 font-weight-medium mb-4 text-dark">唯蟹 Team</h1> <!-- Changed text color back to dark -->
-          <h1 class="hero-1-title font-weight-normal text-dark mb-4">我们不断探索前进</h1> <!-- Changed text color back to dark -->
-          <!-- Added a new paragraph for team introduction -->
+          <h1 class="display-4 font-weight-medium mb-4 text-dark">唯蟹 Team</h1> <!-- 将文本颜色改回深色 -->
+          <h1 class="hero-1-title font-weight-normal text-dark mb-4">我们不断探索前进</h1> <!-- 将文本颜色改回深色 -->
+          <!-- 添加团队介绍 -->
           <p class="lead text-dark">欢迎来到唯蟹 Team，我们是一支热爱探索的团队。通过坚持不懈的努力，我们不断前进，迎接新的挑战。</p>
         </div>
         <div class="col-lg-6 col-md-10">
+          <!-- 成员信息 - 每行显示三个成员 -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="d-flex flex-wrap justify-content-center align-items-center">
+                <!-- 成员1（团队领袖） -->
+                <div class="team-member mx-4">
+                  <a href="https://horatio.cn"><img src="https://q1.qlogo.cn/g?b=qq&nk=1115058004&s=640" alt="Horatio"></a>
+                  <p>Horatio<br>创始人</p>
+                </div>
+                <!-- 成员2（联合创始人） -->
+                <div class="team-member mx-4">
+                  <a href="https://blog.wj0.top"><img src="https://q1.qlogo.cn/g?b=qq&nk=1298800228&s=640" alt="Jack"></a>
+                  <p>Jack<br>联合创始人</p>
+                </div>
+                <!-- 成员3（联合创始人） -->
+                <div class="team-member mx-4">
+                  <a href="https://horatio.cn"><img src="https://q1.qlogo.cn/g?b=qq&nk=3033831844&s=640" alt="Lujing"></a>
+                  <p>Lujing<br>联合创始人</p>
+                </div>
+                <!-- 成员4 -->
+                <div class="team-member mx-4">
+                  <img src="https://q1.qlogo.cn/g?b=qq&nk=3206558616&s=640" alt="Horatio">
+                  <p>Cheng</p>
+                </div>
+                <!-- 成员5 -->
+                <div class="team-member mx-4">
+                  <img src="https://q1.qlogo.cn/g?b=qq&nk=2653382153&s=640" alt="Horatio">
+                  <p>Cheri</p>
+                </div>
+                <!-- 成员6 -->
+                <div class="team-member mx-4">
+                  <img src="https://q1.qlogo.cn/g?b=qq&nk=3211218425&s=640" alt="Horatio">
+                  <p>wdw0</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- 成员信息结束 - 每行显示三个成员 -->
         </div>
       </div>
     </div>
   </section>
-  <!-- Hero End -->
-  <!-- Footer -->
+  <!-- 主体部分结束 -->
+
+  <!-- 页脚 -->
   <footer>
     <?php include 'copyright.php'; ?>
   </footer>
@@ -83,4 +143,5 @@
   <script src="js/scrollspy.min.js"></script>
   <script src="js/jquery.easing.min.js"></script>
 </body>
+
 </html>
