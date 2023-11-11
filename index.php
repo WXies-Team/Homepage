cfaabefd1c9be8808b04422ec257248073bb7cbe
Horatio <hs@horatio.cn>
2023-11-12 02:59:53 +0800

    暂时不知道服务写什么（

--- index.php
+++ index.php
@@ -45,8 +45,9 @@
     /* About us */
     <?php include('about.php'); ?>
 
-    /* Services */
+    /* Services
     <?php include('services.php'); ?>
+    */
 
     /* Footer */
     <?php include('footer-section.php'); ?>
