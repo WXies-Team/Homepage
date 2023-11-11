2825aa59b8ab92b472f5dd79e7b7a655996f86ce
Horatio <hs@horatio.cn>
2023-11-12 03:45:26 +0800

    测试

--- about.php
+++ about.php
@@ -1,57 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        /* 新增的样式 */
+<style>
+    /* Center align the section content */
+    #about {
+        text-align: center;
+    }
 
-        .member-container {
-            text-align: center;
-        }
+    /* Style for team members */
+    .card {
+        border: 0;
+        transition: transform 0.3s ease;
+    }
 
-        .about-img {
-            position: relative;
-        }
+    .card:hover {
+        transform: translateY(-10px);
+    }
 
-        .member-image {
-            width: 100px; /* 调整图片大小 */
-            height: 100px;
-            border-radius: 50%; /* 使图片呈圆形 */
-        }
+    .about-img {
+        overflow: hidden;
+    }
 
-        .member-name {
-            font-size: 18px;
-            font-weight: bold;
-        }
+    .about-img img {
+        width: 100%;
+        height: auto;
+        /* 不再使用border-radius，取消圆形效果 */
+    }
 
-        .member-position {
-            font-size: 13px;
-            text-transform: uppercase;
-            color: #868e96;
-            margin-bottom: 2px;
-        }
+    .mt-3 {
+        margin-top: 1.5rem;
+    }
 
-        /* 剧中显示成员信息和职位 */
+    /* Hyperlink style */
+    h4 a {
+        text-decoration: none;
+        color: #333;
+        /* Link color */
+    }
 
-        .card {
-            text-align: center;
-        }
-
-        /* 超链接样式 */
-
-        .card a {
-            text-decoration: none;
-            color: #007bff;
-        }
-
-        .card a:hover {
-            text-decoration: underline;
-        }
-    </style>
-</head>
-<body>
-
+    h4 a:hover {
+        color: #007bff;
+        /* Hover color */
+    }
+</style>
 <section class="section" id="about">
     <div class="container">
         <div class="row justify-content-center">
@@ -68,11 +56,11 @@
                 <div class="card border-0">
                     <div class="bg-soft-primary about-img rounded">
                         <img src="https://q1.qlogo.cn/g?b=qq&nk=1115058004&s=640" alt="Horatio"
-                             class="img-fluid d-block mx-auto member-image">
+                            class="img-fluid d-block mx-auto">
                     </div>
                     <div class="mt-3">
-                        <p class="text-uppercase text-muted mb-2 f-13 member-position">创始人</p>
-                        <h4 class="f-18 member-name"><a href="https://horatio.cn/">Horatio</a></h4>
+                        <p class="text-uppercase text-muted mb-2 f-13">创始人</p>
+                        <h4 class="f-18"><a href="https://horatio.cn/">Horatio</a></h4>
                     </div>
                 </div>
             </div>
@@ -82,11 +70,11 @@
                 <div class="card border-0">
                     <div class="bg-soft-primary about-img rounded">
                         <img src="https://q1.qlogo.cn/g?b=qq&nk=1298800228&s=640" alt="Jack"
-                             class="img-fluid d-block mx-auto member-image">
+                            class="img-fluid d-block mx-auto">
                     </div>
                     <div class="mt-3">
-                        <p class="text-uppercase text-muted mb-2 f-13 member-position">联合创始人</p>
-                        <h4 class="f-18 member-name"><a href="https://blog.wj0.top/">Jack</a></h4>
+                        <p class="text-uppercase text-muted mb-2 f-13">联合创始人</p>
+                        <h4 class="f-18"><a href="https://blog.wj0.top/">Jack</a></h4>
                     </div>
                 </div>
             </div>
@@ -96,11 +84,11 @@
                 <div class="card border-0">
                     <div class="bg-soft-primary about-img rounded">
                         <img src="https://q1.qlogo.cn/g?b=qq&nk=3033831844&s=640" alt="Jack"
-                             class="img-fluid d-block mx-auto member-image">
+                            class="img-fluid d-block mx-auto">
                     </div>
                     <div class="mt-3">
-                        <p class="text-uppercase text-muted mb-2 f-13 member-position">联合创始人</p>
-                        <h4 class="f-18 member-name"><a href="/">Lujing</a></h4>
+                        <p class="text-uppercase text-muted mb-2 f-13">联合创始人</p>
+                        <h4 class="f-18"><a href="/">Lujing</a></h4>
                     </div>
                 </div>
             </div>
@@ -110,11 +98,11 @@
                 <div class="card border-0">
                     <div class="bg-soft-primary about-img rounded">
                         <img src="https://q1.qlogo.cn/g?b=qq&nk=3206558616&s=640" alt="Cheng"
-                             class="img-fluid d-block mx-auto member-image">
+                            class="img-fluid d-block mx-auto">
                     </div>
                     <div class="mt-3">
-                        <p class="text-uppercase text-muted mb-2 f-13 member-position">成员</p>
-                        <h4 class="f-18 member-name"><a href="/">Cheng</a></h4>
+                        <p class="text-uppercase text-muted mb-2 f-13">成员</p>
+                        <h4 class="f-18"><a href="/">Cheng</a></h4>
                     </div>
                 </div>
             </div>
@@ -124,11 +112,11 @@
                 <div class="card border-0">
                     <div class="bg-soft-primary about-img rounded">
                         <img src="https://q1.qlogo.cn/g?b=qq&nk=2653382153&s=640" alt="Cheri"
-                             class="img-fluid d-block mx-auto member-image">
+                            class="img-fluid d-block mx-auto">
                     </div>
                     <div class="mt-3">
-                        <p class="text-uppercase text-muted mb-2 f-13 member-position">成员</p>
-                        <h4 class="f-18 member-name"><a href="/">Cheri</a></h4>
+                        <p class="text-uppercase text-muted mb-2 f-13">成员</p>
+                        <h4 class="f-18"><a href="/">Cheri</a></h4>
                     </div>
                 </div>
             </div>
@@ -138,17 +126,14 @@
                 <div class="card border-0">
                     <div class="bg-soft-primary about-img rounded">
                         <img src="https://q1.qlogo.cn/g?b=qq&nk=3211218425&s=640" alt="wdw0"
-                             class="img-fluid d-block mx-auto member-image">
+                            class="img-fluid d-block mx-auto">
                     </div>
                     <div class="mt-3">
-                        <p class="text-uppercase text-muted mb-2 f-13 member-position">成员</p>
-                        <h4 class="f-18 member-name"><a href="/">wdw0</a></h4>
+                        <p class="text-uppercase text-muted mb-2 f-13">成员</p>
+                        <h4 class="f-18"><a href="/">wdw0</a></h4>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-</body>
-</html>
