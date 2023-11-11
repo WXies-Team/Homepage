e5d8a4a86364f4a63319435aced4abc6bcfe1247
Horatio <hs@horatio.cn>
2023-11-12 03:47:37 +0800

    回滚

--- about.php
+++ about.php
@@ -50,92 +50,88 @@
             </div>
         </div>
 
+        <div class="row">
             <!-- Team Member 1 -->
             <div class="col-lg-4">
                 <div class="card border-0">
-                    <a href="https://horatio.cn/"><!-- 超链接包围整个卡片 -->
-                        <div class="bg-soft-primary about-img rounded">
-                            <img src="https://q1.qlogo.cn/g?b=qq&nk=1115058004&s=640" alt="Horatio" class="img-fluid d-block mx-auto">
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-uppercase text-muted mb-2 f-13">创始人</p>
-                            <h4 class="f-18">Horatio</h4>
-                        </div>
-                    </a>
+                    <div class="bg-soft-primary about-img rounded">
+                        <img src="https://q1.qlogo.cn/g?b=qq&nk=1115058004&s=640" alt="Horatio"
+                            class="img-fluid d-block mx-auto">
+                    </div>
+                    <div class="mt-3">
+                        <p class="text-uppercase text-muted mb-2 f-13">创始人</p>
+                        <h4 class="f-18"><a href="https://horatio.cn/">Horatio</a></h4>
+                    </div>
                 </div>
             </div>
 
             <!-- Team Member 2 -->
             <div class="col-lg-4">
                 <div class="card border-0">
-                    <a href="https://blog.wj0.top/"><!-- 超链接包围整个卡片 -->
-                        <div class="bg-soft-primary about-img rounded">
-                            <img src="https://q1.qlogo.cn/g?b=qq&nk=1298800228&s=640" alt="Jack" class="img-fluid d-block mx-auto">
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-uppercase text-muted mb-2 f-13">联合创始人</p>
-                            <h4 class="f-18">Jack</h4>
-                        </div>
-                    </a>
+                    <div class="bg-soft-primary about-img rounded">
+                        <img src="https://q1.qlogo.cn/g?b=qq&nk=1298800228&s=640" alt="Jack"
+                            class="img-fluid d-block mx-auto">
+                    </div>
+                    <div class="mt-3">
+                        <p class="text-uppercase text-muted mb-2 f-13">联合创始人</p>
+                        <h4 class="f-18"><a href="https://blog.wj0.top/">Jack</a></h4>
+                    </div>
                 </div>
             </div>
 
             <!-- Team Member 3 -->
             <div class="col-lg-4">
                 <div class="card border-0">
-                    <a href="/"><!-- 超链接包围整个卡片 -->
-                        <div class="bg-soft-primary about-img rounded">
-                            <img src="https://q1.qlogo.cn/g?b=qq&nk=3033831844&s=640" alt="Lujing" class="img-fluid d-block mx-auto">
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-uppercase text-muted mb-2 f-13">联合创始人</p>
-                            <h4 class="f-18">Lujing</h4>
-                        </div>
-                    </a>
+                    <div class="bg-soft-primary about-img rounded">
+                        <img src="https://q1.qlogo.cn/g?b=qq&nk=3033831844&s=640" alt="Jack"
+                            class="img-fluid d-block mx-auto">
+                    </div>
+                    <div class="mt-3">
+                        <p class="text-uppercase text-muted mb-2 f-13">联合创始人</p>
+                        <h4 class="f-18"><a href="/">Lujing</a></h4>
+                    </div>
                 </div>
             </div>
 
             <!-- Team Member 4 -->
             <div class="col-lg-4">
                 <div class="card border-0">
-                    <a href="/"><!-- 超链接包围整个卡片 -->
-                        <div class="bg-soft-primary about-img rounded">
-                            <img src="https://q1.qlogo.cn/g?b=qq&nk=3206558616&s=640" alt="Cheng" class="img-fluid d-block mx-auto">
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-uppercase text-muted mb-2 f-13">成员</p>
-                            <h4 class="f-18">Cheng</h4>
-                        </div>
-                    </a>
+                    <div class="bg-soft-primary about-img rounded">
+                        <img src="https://q1.qlogo.cn/g?b=qq&nk=3206558616&s=640" alt="Cheng"
+                            class="img-fluid d-block mx-auto">
+                    </div>
+                    <div class="mt-3">
+                        <p class="text-uppercase text-muted mb-2 f-13">成员</p>
+                        <h4 class="f-18"><a href="/">Cheng</a></h4>
+                    </div>
                 </div>
             </div>
 
             <!-- Team Member 5 -->
             <div class="col-lg-4">
                 <div class="card border-0">
-                    <a href="/"><!-- 超链接包围整个卡片 -->
-                        <div class="bg-soft-primary about-img rounded">
-                            <img src="https://q1.qlogo.cn/g?b=qq&nk=2653382153&s=640" alt="Cheri" class="img-fluid d-block mx-auto">
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-uppercase text-muted mb-2 f-13">成员</p>
-                            <h4 class="f-18">Cheri</h4>
-                        </div>
-                    </a>
+                    <div class="bg-soft-primary about-img rounded">
+                        <img src="https://q1.qlogo.cn/g?b=qq&nk=2653382153&s=640" alt="Cheri"
+                            class="img-fluid d-block mx-auto">
+                    </div>
+                    <div class="mt-3">
+                        <p class="text-uppercase text-muted mb-2 f-13">成员</p>
+                        <h4 class="f-18"><a href="/">Cheri</a></h4>
+                    </div>
                 </div>
             </div>
 
             <!-- Team Member 6 -->
             <div class="col-lg-4">
                 <div class="card border-0">
-                    <a href="/"><!-- 超链接包围整个卡片 -->
-                        <div class="bg-soft-primary about-img rounded">
-                            <img src="https://q1.qlogo.cn/g?b=qq&nk=3211218425&s=640" alt="wdw0" class="img-fluid d-block mx-auto">
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-uppercase text-muted mb-2 f-13">成员</p>
-                            <h4 class="f-18">wdw0</h4>
-                        </div>
+                    <div class="bg-soft-primary about-img rounded">
+                        <img src="https://q1.qlogo.cn/g?b=qq&nk=3211218425&s=640" alt="wdw0"
+                            class="img-fluid d-block mx-auto">
+                    </div>
+                    <div class="mt-3">
+                        <p class="text-uppercase text-muted mb-2 f-13">成员</p>
+                        <h4 class="f-18"><a href="/">wdw0</a></h4>
+                    </div>
                 </div>
             </div>
         </div>
