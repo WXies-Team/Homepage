a80df9c68e1a2bcca1ffdd2c6e0ab7db3fb1cdcd
Horatio <hs@horatio.cn>
2023-11-12 03:54:22 +0800

    1

--- services.php
+++ services.php
@@ -4,7 +4,6 @@
             <div class="col-lg-7">
                 <div class="text-center mb-5">
                     <h2 class="">服务</h2>
-                    <P class="text-muted">还没想好怎么写</P>
                 </div>
             </div>
         </div>
@@ -17,8 +16,8 @@
                             <img src="images/services-icon/icon-1.png" alt="" class="img-fluid d-block">
                         </div>
                     </div>
-                    <h4 class="service-title mt-4 mb-3 f-18"></h4>
-                    <p class="service-subtitle mb-4 f-15"></p>
+                    <h4 class="service-title mt-4 mb-3 f-18">用户支持准则 - "代码搭救超能力"</h4>
+                    <p class="service-subtitle mb-4 f-15">如果您在代码的迷宫中迷失方向，我们会派遣虚拟指南企鹅为您指路，因为在互联网世界里，企鹅是最佳导航员。</p>
                 </div>
             </div>
             <div class="col-lg-4">
@@ -29,8 +28,8 @@
                             <img src="images/services-icon/icon-2.png" alt="" class="img-fluid d-block">
                         </div>
                     </div>
-                    <h4 class="service-title mt-4 mb-3 f-18"></h4>
-                    <p class="service-subtitle mb-4 f-15"></p>
+                    <h4 class="service-title mt-4 mb-3 f-18">网络安全准则 - "数据保卫战"</h4>
+                    <p class="service-subtitle mb-4 f-15">如果有人试图攻击我们的系统，我们会发动虚拟恐龙大军，因为在互联网的世界里，只有恐龙能够真正保护数字秘境。</p>
                 </div>
             </div>
             <div class="col-lg-4">
@@ -41,8 +40,8 @@
                             <img src="images/services-icon/icon-3.png" alt="" class="img-fluid d-block">
                         </div>
                     </div>
-                    <h4 class="service-title mt-4 mb-3 f-18"></h4>
-                    <p class="service-subtitle mb-4 f-15"></p>
+                    <h4 class="service-title mt-4 mb-3 f-18">数据分析准则 - "数据解读比拯救世界还伟大"</h4>
+                    <p class="service-subtitle mb-4 f-15">如果您对数据感到困惑，我们会派发虚拟翻译鹦鹉，因为在数字语境中，只有鹦鹉能够真正理解数据的语言。</p>
                 </div>
             </div>
         </div>
