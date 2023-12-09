562f6cb376009e885516da31ebfd83d09a2dbe7f
Horatio <hs@horatio.cn>
2023-12-09 15:37:39 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,22 +10,32 @@
                 <div class="col-lg-7 offset-lg-1">
                     <div class="row">
                         <div class="col-md-4">
-                            <h4 class="text-white f-22 font-weight-normal mb-3">产品</h4>
+                            <h4 class="text-white f-22 font-weight-normal mb-3"> </h4>
                             <ul class="list-unstyled footer-sub-menu">
-                                <li><a href="#" class="footer-link">Works</a></li>
-                                <li><a href="#" class="footer-link">Strategy</a></li>
-                                <li><a href="#" class="footer-link">Releases</a></li>
-                                <li><a href="#" class="footer-link">Press</a></li>
-                                <li><a href="#" class="footer-link">Mission</a></li>
+                                <li><a href="#" class="footer-link"> </a></li>
+                                <li><a href="#" class="footer-link"> </a></li>
+                                <li><a href="#" class="footer-link"> </a></li>
+                                <li><a href="#" class="footer-link"> </a></li>
+                                <li><a href="#" class="footer-link"> </a></li>
                             </ul>
                         </div>
                         <div class="col-md-4">
-                            <h4 class="text-white f-22 font-weight-normal mb-3">文档</h4>
+                            <h4 class="text-white f-22 font-weight-normal mb-3">服务</h4>
                             <ul class="list-unstyled footer-sub-menu">
                                 <li><a href="#" class="footer-link">Tranding</a></li>
                                 <li><a href="#" class="footer-link">Popular</a></li>
                                 <li><a href="#" class="footer-link">Customers</a></li>
                                 <li><a href="#" class="footer-link">Features</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <h4 class="text-white f-22 font-weight-normal mb-3">文档</h4>
+                            <ul class="list-unstyled footer-sub-menu">
+                                <li><a href="#" class="footer-link">Developers</a></li>
+                                <li><a href="#" class="footer-link">Support</a></li>
+                                <li><a href="#" class="footer-link">Customer Service</a></li>
+                                <li><a href="#" class="footer-link">Get Started</a></li>
+                                <li><a href="#" class="footer-link">Guide</a></li>
                             </ul>
                         </div>
                     </div>
