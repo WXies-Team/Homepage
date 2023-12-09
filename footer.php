d5e77c38947e29badb9536369f1009770b4b5bf7
Horatio <hs@horatio.cn>
2023-12-09 15:26:33 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
                 <div class="col-lg-7 offset-lg-1">
                     <div class="row">
                         <div class="col-md-4">
-                            <h4 class="text-white f-22 font-weight-normal mb-3">Customer</h4>
+                            <h4 class="text-white f-22 font-weight-normal mb-3">产品</h4>
                             <ul class="list-unstyled footer-sub-menu">
                                 <li><a href="#" class="footer-link">Works</a></li>
                                 <li><a href="#" class="footer-link">Strategy</a></li>
@@ -20,22 +20,12 @@
                             </ul>
                         </div>
                         <div class="col-md-4">
-                            <h4 class="text-white f-22 font-weight-normal mb-3">Product</h4>
+                            <h4 class="text-white f-22 font-weight-normal mb-3">联系</h4>
                             <ul class="list-unstyled footer-sub-menu">
                                 <li><a href="#" class="footer-link">Tranding</a></li>
                                 <li><a href="#" class="footer-link">Popular</a></li>
                                 <li><a href="#" class="footer-link">Customers</a></li>
                                 <li><a href="#" class="footer-link">Features</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-md-4">
-                            <h4 class="text-white f-22 font-weight-normal mb-3">Learn More</h4>
-                            <ul class="list-unstyled footer-sub-menu">
-                                <li><a href="#" class="footer-link">Developers</a></li>
-                                <li><a href="#" class="footer-link">Support</a></li>
-                                <li><a href="#" class="footer-link">Customer Service</a></li>
-                                <li><a href="#" class="footer-link">Get Started</a></li>
-                                <li><a href="#" class="footer-link">Guide</a></li>
                             </ul>
                         </div>
                     </div>
