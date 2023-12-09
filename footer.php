ef46886f545795740cf63e957777478099841aef
Horatio <hs@horatio.cn>
2023-12-09 15:20:25 +0800

    尝试修改页脚样式

--- footer.php
+++ footer.php
@@ -1,16 +1,3 @@
-<style>
-    /* 样式，将版权信息居中显示在白色背景上 */
-    .copyright-wrapper {
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        background-color: white;
-        padding: 10px 0;
-        text-align: center;
-    }
-</style>
-
 <div class="copyright-wrapper">
     <!-- 使用PHP的date()函数获取当前年份并显示在版权信息中 -->
     <p>©
