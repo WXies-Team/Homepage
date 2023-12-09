2837b92b518ee0d489523367571282fa6faccf3b
Horatio <hs@horatio.cn>
2023-12-09 15:38:44 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -12,30 +12,17 @@
                         <div class="col-md-4">
                             <h4 class="text-white f-22 font-weight-normal mb-3"> </h4>
                             <ul class="list-unstyled footer-sub-menu">
-                                <li><a href="#" class="footer-link"> </a></li>
-                                <li><a href="#" class="footer-link"> </a></li>
-                                <li><a href="#" class="footer-link"> </a></li>
-                                <li><a href="#" class="footer-link"> </a></li>
-                                <li><a href="#" class="footer-link"> </a></li>
                             </ul>
                         </div>
                         <div class="col-md-4">
                             <h4 class="text-white f-22 font-weight-normal mb-3">服务</h4>
                             <ul class="list-unstyled footer-sub-menu">
-                                <li><a href="#" class="footer-link">Tranding</a></li>
-                                <li><a href="#" class="footer-link">Popular</a></li>
-                                <li><a href="#" class="footer-link">Customers</a></li>
-                                <li><a href="#" class="footer-link">Features</a></li>
-                            </ul>
+                                <li><a href="#" class="footer-link">敬请期待</a></li>
                         </div>
                         <div class="col-md-4">
                             <h4 class="text-white f-22 font-weight-normal mb-3">文档</h4>
                             <ul class="list-unstyled footer-sub-menu">
-                                <li><a href="#" class="footer-link">Developers</a></li>
-                                <li><a href="#" class="footer-link">Support</a></li>
-                                <li><a href="#" class="footer-link">Customer Service</a></li>
-                                <li><a href="#" class="footer-link">Get Started</a></li>
-                                <li><a href="#" class="footer-link">Guide</a></li>
+                                <li><a href="#" class="footer-link">敬请期待</a></li>
                             </ul>
                         </div>
                     </div>
