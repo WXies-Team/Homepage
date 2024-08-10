27bbb3ec2f42c047a7636da61d3f7e1332b05bed
Horatio <hs@horatio.cn>
2024-08-10 16:36:15 +0800

    Update head.php

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8" />
-    <title>唯蟹 Team - We Quest On and On</title>
+    <title>test唯蟹 Team - We Quest On and On</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="We Quest On and On">
     <meta name="keywords" content="唯蟹Team, Xies' Group, Blog233, HoratioWeb, lujing, jack">
